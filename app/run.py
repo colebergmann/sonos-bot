6274f0b379ecadae1ac9c5328d902637e46081e8
Cole Bergmann <43765292+colebergmann@users.noreply.github.com>
2020-02-06 23:59:49 -0800

    cb - added date field and endpoint for setup submissions

--- app/run.py
+++ app/run.py
@@ -1,5 +1,6 @@
 from flask import render_template, Flask, request
 from Robot import Robot
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -29,12 +30,33 @@ def test():
 
 @app.route('/setup/submit', methods=['POST'])
 def submit_params():
-    #if (request.args.get("lat") == None || request.args.get("long") == None
-    return request.args
+    if request.json["lat"] is None or request.json["long"] is None or request.json["elevation"] is None \
+            or request.json["date"] is None:
+        return {"error": "Please fill out all the required fields"}
+    if is_number(request.json["lat"]) is False or is_number(request.json["long"]) is False:
+        return {"error": "Latitude and longitude must be numbers"}
+    if is_number(request.json["elevation"]) is False:
+        return {"error": "Elevation must be a number"}
+    try:
+        pd.to_datetime(request.json["date"], errors='raise')
+    except ValueError:
+        return {"error": "Invalid date: date must be in format MM/DD/YYYY"}
+
+    # if we got here, then all the inputs were valid
+    # kick off the program and return a success message
+    return {"status":"success"}
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 
 # start the webserver
 if __name__ == "__main__":
     robot = Robot()
     app.debug = True
-    app.run(use_reloader=False)
+    app.run(use_reloader=False, host='0.0.0.0')
--- app/test.py
+++ app/test.py
@@ -1,2 +1,8 @@
-from Robot import Robot
-robot = Robot()
+#from Robot import Robot
+import pandas as pd
+#robot = Robot()
+try:
+    pd.to_datetime("2ok", errors='raise')
+    # do something
+except ValueError:
+    print("invalid")
