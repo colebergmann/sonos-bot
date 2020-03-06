85c525f0b8baaaf73c916791bd815053b08fde30
Cole Bergmann <43765292+colebergmann@users.noreply.github.com>
2020-03-05 19:36:50 -0800

    cb - fixed model test file

--- app/test.py
+++ app/test.py
@@ -1,14 +1,7 @@
-#from Robot import Robot
-import pandas as pd
 from Model import Model
 
-mdl = Model(12, 12, 6200, '06/05/1999')
-#mdl = Model(34.42,-119.82, 6200, '2019-12-06')
+#mdl = Model(12, 12, 6200, '06/05/1999') # why is az vertical in the middle of the day? # it inverts after 0 deg
+mdl = Model(34.42,-119.82, 6200, '2019-12-06')
 
-# wtf why is az vertical in the middle of the day?
-
-mdl.trim()
+mdl.trim() # trim the plots to one full cycle of apparent_elevation
 mdl.show_plots()
-for i in mdl.get_azimuth_arr():
-    print(i)
-
